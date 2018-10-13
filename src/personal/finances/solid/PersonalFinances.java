fdc3bcde7c27a3ff4a97e4629e2c8ffa64aec931
geraJuarez <geratrex781@gmail.com>
2018-10-13 17:16:30 -0500

    Moved main code into a class

---
+++ src/personal/finances/solid/PersonalFinances.java
@@ -0,0 +1,86 @@
+package personal.finances.solid;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Zegerd
+ */
+public class PersonalFinances {
+    private Scanner scanner;
+    private int todayPurchases;
+    private String[] payees;
+    private float[] amounts;
+    
+    Map<String, Integer> payeesCount;
+    float min;
+    float max;
+    float avg;
+    
+    String frequentPayee;
+    Date today;
+    
+    public PersonalFinances(){
+        this.scanner = new Scanner(System.in);
+        this.todayPurchases = 0;
+        
+        payeesCount = new HashMap<>();
+        min = Float.MAX_VALUE;
+        max = 0;
+        avg = 0;
+        
+        frequentPayee = "";
+        today = Calendar.getInstance().getTime();
+    }
+    
+    public void readData() {
+        System.out.println("How many purchases did you did today?");
+        todayPurchases = this.scanner.nextInt();
+        
+        payees = new String[todayPurchases];
+        amounts = new float[todayPurchases];
+        
+        for (int i = 0; i < todayPurchases; i++) {
+            payees[i] = scanner.next();
+            amounts[i] = scanner.nextFloat();
+        }
+    }
+    
+    public void calculateInformation() {
+       
+        for (int i = 0; i < todayPurchases; i++){
+            if (min > amounts[i]){
+                min = amounts[i];
+            }
+            if (max < amounts[i]){
+                 max = amounts[i];
+            }
+            avg += amounts[i];
+            payeesCount.put(payees[i], payeesCount.getOrDefault(payees[i], 0) + 1);
+        }
+
+        avg /= todayPurchases;
+        
+        int maxPayeeFreq = 0;
+        for (String key: payeesCount.keySet()) {
+            int freq = payeesCount.get(key);
+            if (maxPayeeFreq < freq) {
+                maxPayeeFreq = freq;
+                frequentPayee = key;
+            }
+        }
+    }
+    
+    public void printData() {
+        System.out.println("Purchases of " + today);
+        System.out.println("Min: " + min);
+        System.out.println("Max: " + max);
+        System.out.println("Avg: " + avg);
+        System.out.println("Frequent Payee: " + frequentPayee);
+    }
+    
+}
--- src/personal/finances/solid/PersonalFinancesSOLID.java
+++ src/personal/finances/solid/PersonalFinancesSOLID.java
@@ -12,53 +12,11 @@ import java.util.Scanner;
  */
 public class PersonalFinancesSOLID {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("How many purchases did you did today?");
-        
-        int todayPurchases = scanner.nextInt();
-        String[] payees = new String[todayPurchases];
-        float[] amounts = new float[todayPurchases];
-        
-        for (int i = 0; i < todayPurchases; i++) {
-            payees[i] = scanner.next();
-            amounts[i] = scanner.nextFloat();
-        }
-        
-        Map<String, Integer> payeesCount = new HashMap<>();
-        float min = Float.MAX_VALUE;
-        float max = 0;
-        float avg = 0;
-        
-        for (int i = 0; i < todayPurchases; i++){
-            if (min > amounts[i]){
-                min = amounts[i];
-            }
-            if (max < amounts[i]){
-                 max = amounts[i];
-            }
-            avg += amounts[i];
-            payeesCount.put(payees[i], payeesCount.getOrDefault(payees[i], 0) + 1);
-        }
-
-        avg /= todayPurchases;
-        String frequentPayee = "";
-        int maxPayeeFreq = 0;
-        Date today = Calendar.getInstance().getTime();
-        
-        for (String key: payeesCount.keySet()) {
-            int freq = payeesCount.get(key);
-            if (maxPayeeFreq < freq) {
-                maxPayeeFreq = freq;
-                frequentPayee = key;
-            }
-        }
-        
-        System.out.println("Purchases of " + today);
-        System.out.println("Min: " + min);
-        System.out.println("Max: " + max);
-        System.out.println("Avg: " + avg);
-        System.out.println("Frequent Payee: " + frequentPayee);
+       
+        PersonalFinances pf = new PersonalFinances();
+        pf.readData();
+        pf.calculateInformation();
+        pf.printData();
     }
     
 }
